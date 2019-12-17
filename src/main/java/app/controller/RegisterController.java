cc282442341641d71e8dc6be58bb7ab033525903
Isgkha <xalidisgenderov1@gmail.com>
2019-12-17 14:46:39 +0400

    register and login

--- src/main/java/app/controller/RegisterController.java
+++ src/main/java/app/controller/RegisterController.java
@@ -2,6 +2,7 @@ package app.controller;
 
 import app.service.RegisterService;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -13,6 +14,7 @@ public class RegisterController {
     public RegisterController(RegisterService registerService) {
         this.registerService = registerService;
     }
+    @PostMapping
     public String registerPost(@RequestParam("name") String name,@RequestParam("surname") String surname,@RequestParam("email") String email,
                                @RequestParam("password") String password,@RequestParam("radiobox") String radiobox,
                                @RequestParam("gender") String gender,@RequestParam("phoneNum") String phoneNum,
