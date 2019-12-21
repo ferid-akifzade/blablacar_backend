42effae640fe5fd9b33172836fa40dd74dd8029d
temet_nosce <akifzade.ferid1995@gmail.com>
2019-12-21 21:20:57 +0400

    -- bugs fixed

--- src/main/java/app/service/ReservationService.java
+++ src/main/java/app/service/ReservationService.java
@@ -1,8 +1,10 @@
 package app.service;
 
 import app.libs.Driver;
+import app.libs.History;
 import app.libs.Vehicle;
 import app.repository.DriverRepository;
+import app.repository.HistoryRepository;
 import app.repository.RideRepository;
 import app.repository.VehicleRepository;
 import org.springframework.stereotype.Service;
@@ -14,17 +16,18 @@ public class ReservationService {
     private final RideRepository rideRepository;
     private final VehicleRepository vehicleRepository;
     private final DriverRepository driverRepository;
+    private final HistoryRepository historyRepository;
     private boolean found = false;
 
-    public ReservationService(RideRepository rideRepository, VehicleRepository vehicleRepository, DriverRepository driverRepository) {
+    public ReservationService(RideRepository rideRepository, VehicleRepository vehicleRepository, DriverRepository driverRepository, HistoryRepository historyRepository) {
         this.rideRepository = rideRepository;
         this.vehicleRepository = vehicleRepository;
         this.driverRepository = driverRepository;
+        this.historyRepository = historyRepository;
     }
 
 
-    public boolean reservation(int rideID, int seats, int clientID) {
-
+    public Optional<History> reservation(int rideID, int seats, int clientID) {
         rideRepository.findById(rideID).stream()
                 .filter(e -> {
                     Optional<Driver> driverId = driverRepository.findById(e.getDriver_id());
@@ -36,13 +39,12 @@ public class ReservationService {
             Optional<Driver> driverId = driverRepository.findById(ride.getDriver_id());
             Optional<Vehicle> vehicleOpt = vehicleRepository.findById(driverId.orElse(new Driver("", "", "", "", "", "", -1)).getVehicle_id());
             Vehicle vehicle = vehicleOpt.orElse(new Vehicle("", -2));
-            vehicle.setSeats(vehicle.getSeats() - seats);;
+            vehicle.setSeats(vehicle.getSeats() - seats);
             vehicleRepository.save(vehicle);
             found = true;
-
-
         });
-
-        return found;
+        if (found)
+            return Optional.of(historyRepository.save(new History(clientID, rideID, seats)));
+        return Optional.empty();
     }
 }
