9f64050c4503730ff709fac30930750470cc6eac
temet_nosce <akifzade.ferid1995@gmail.com>
2019-12-22 23:45:19 +0400

    -- updated

--- src/main/java/app/service/SearchService.java
+++ src/main/java/app/service/SearchService.java
@@ -23,14 +23,7 @@ public class SearchService {
         this.driverRepository = driverRepository;
     }
 
-    // custom methods spring data
-    public List<HashMap<String, Object>> findAll(String departure, String destination, String datetime) {
-        Iterable<Ride> all = rideRepository.findAll();
-        List<Ride> collect = StreamSupport.stream(all.spliterator(), false)
-                .filter(e -> e.getFrom_place().equals(departure))
-                .filter(e -> e.getTo_place().equals(destination))
-                .filter(e -> e.getDate().equals(datetime))
-                .collect(Collectors.toList());
+    private List<HashMap<String, Object>> getComprehensiveData(List<Ride> collect) {
         List<HashMap<String, Object>> data = new LinkedList<>();
         for (Ride oneRide : collect) {
             HashMap<String, Object> tmp = new HashMap<>();
@@ -53,27 +46,18 @@ public class SearchService {
         return data;
     }
 
+    // custom methods spring data
+    public List<HashMap<String, Object>> findAll(String departure, String destination, String datetime) {
+        Iterable<Ride> all = rideRepository.findAll();
+        List<Ride> collect = StreamSupport.stream(all.spliterator(), false)
+                .filter(e -> e.getFrom_place().equals(departure))
+                .filter(e -> e.getTo_place().equals(destination))
+                .filter(e -> e.getDate().equals(datetime))
+                .collect(Collectors.toList());
+        return getComprehensiveData(collect);
+    }
+
     public List<HashMap<String, Object>> getAll() {
-        List<Ride> all = (List<Ride>) rideRepository.findAll();
-        List<HashMap<String, Object>> data = new LinkedList<>();
-        for (Ride oneRide : all) {
-            HashMap<String, Object> tmp = new HashMap<>();
-            tmp.put("id", oneRide.getId());
-            tmp.put("from_place", oneRide.getFrom_place());
-            tmp.put("to_place", oneRide.getTo_place());
-            tmp.put("comment", oneRide.getComment());
-            tmp.put("date", oneRide.getDate());
-            tmp.put("price", oneRide.getPrice());
-            Optional<Driver> driverOPT = driverRepository.findById(oneRide.getDriver_id());
-            driverOPT.ifPresent(driver -> {
-                tmp.put("name", driver.getName());
-                tmp.put("surname", driver.getSurname());
-                tmp.put("gender", driver.getGender());
-                tmp.put("phonenum", driver.getPhonenum());
-                tmp.put("vehicle_id", driver.getVehicle_id());
-            });
-            data.add(tmp);
-        }
-        return data;
+        return getComprehensiveData((List<Ride>) rideRepository.findAll());
     }
 }
