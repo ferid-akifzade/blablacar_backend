9b20b14749b234ae46fe325136d4c86d6614b629
temet_nosce <akifzade.ferid1995@gmail.com>
2019-12-22 22:52:07 +0400

    -- updated

--- src/main/java/app/service/SearchService.java
+++ src/main/java/app/service/SearchService.java
@@ -1,12 +1,15 @@
 package app.service;
 
+import app.libs.Driver;
 import app.libs.Ride;
 import app.repository.DriverRepository;
 import app.repository.RideRepository;
-import app.repository.VehicleRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Optional;
 import java.util.stream.Collectors;
 import java.util.stream.StreamSupport;
 
@@ -14,22 +17,40 @@ import java.util.stream.StreamSupport;
 public class SearchService {
     private final RideRepository rideRepository;
     private final DriverRepository driverRepository;
-    private final VehicleRepository vehicleRepository;
 
-    public SearchService(RideRepository rideRepository, DriverRepository driverRepository, VehicleRepository vehicleRepository) {
+    public SearchService(RideRepository rideRepository, DriverRepository driverRepository) {
         this.rideRepository = rideRepository;
         this.driverRepository = driverRepository;
-        this.vehicleRepository = vehicleRepository;
     }
 
     // custom methods spring data
-    public List<Ride> findAll(String departure, String destination, String datetime) {
+    public List<HashMap<String, Object>> findAll(String departure, String destination, String datetime) {
         Iterable<Ride> all = rideRepository.findAll();
-        return StreamSupport.stream(all.spliterator(), false)
+        List<Ride> collect = StreamSupport.stream(all.spliterator(), false)
                 .filter(e -> e.getFrom_place().equals(departure))
                 .filter(e -> e.getTo_place().equals(destination))
                 .filter(e -> e.getDate().equals(datetime))
                 .collect(Collectors.toList());
+        List<HashMap<String, Object>> data = new LinkedList<>();
+        for (Ride oneRide : collect) {
+            HashMap<String, Object> tmp = new HashMap<>();
+            tmp.put("id", oneRide.getId());
+            tmp.put("from_place", oneRide.getFrom_place());
+            tmp.put("to_place", oneRide.getTo_place());
+            tmp.put("comment", oneRide.getComment());
+            tmp.put("date", oneRide.getDate());
+            tmp.put("price", oneRide.getPrice());
+            Optional<Driver> driverOPT = driverRepository.findById(oneRide.getDriver_id());
+            driverOPT.ifPresent(driver -> {
+                tmp.put("name", driver.getName());
+                tmp.put("surname", driver.getSurname());
+                tmp.put("gender", driver.getGender());
+                tmp.put("phonenum", driver.getPhonenum());
+                tmp.put("vehicle_id", driver.getVehicle_id());
+            });
+            data.add(tmp);
+        }
+        return data;
     }
 
     public List<Ride> getAll() {
